542c4a3062f5f10839aa63cc22391a1f4523b016
Sploob <crayiphone@gmail.com>
2022-03-21 13:20:24 -0700

    add subtraction, multiplication and division functions
    Co-authored-by: Meron Tekie <mtekiesd210309@gmail.com>
    Co-authored-by: Dylan Peterson <dylan.peterson17@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
+// UI Logic
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = divide(number1, number2);
+
+// Business Logic
+
 function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
+function multiply(number1, number2) {
+  return number1 * number2;
+}
 
-alert(add(number1, number2));
+function divide(number1, number2){
+  return number1 / number2;
+}
+
+alert(result);
